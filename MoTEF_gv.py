f3de57c288f092de7a9a447d2d8e7e32c9be3fb0
Monseej Purkayastha <60354660+m-gigaton@users.noreply.github.com>
2024-07-07 12:15:02 +0200

    Updated script

---
+++ MoTEF_gv.py
@@ -0,0 +1,271 @@
+"""
+implementation of the MoTEF federated learning algorithm (https://arxiv.org/pdf/2405.20114)
+compare with document MoTEF_Alg.pdf
+part of the course work for optmization in machine learning
+"""
+from model.resnet8 import *
+import torch
+import torch.nn as nn
+import torch.distributed as dist
+import torch.multiprocessing as mp
+from torch.utils.data import DataLoader
+from torch.nn.parallel import DistributedDataParallel as DDP 
+import torchvision
+import torchvision.transforms as transforms
+
+import time
+
+import os
+
+model = ResNet8()
+
+
+
+
+def setup(rank, world_size):
+    print(f'Setup started for {rank}, {torch.cuda.is_available()}')
+    # os.environ['MASTER_ADDR'] = 'localhost'
+    # os.environ['MASTER_ADDR'] = os.environ['SLURM_SUBMIT_HOST']
+    os.environ['MASTER_PORT'] = '12365'
+    # set backend manually for now to avoid problems with shared GPU
+    # backend = 'gloo'
+
+    torch.cuda.set_device(rank)
+
+    backend = 'NCCL' if torch.cuda.is_available() else 'gloo'
+    dist.init_process_group(backend=backend, rank=rank, world_size=world_size)
+    print(f'setup for {rank}')
+
+def cleanup():
+    dist.destroy_process_group()
+
+
+def communicate_with_neighbors(rank, world_size, q_h_i, q_g_i):
+    device = q_h_i.device
+    left_neighbor = (rank - 1) % world_size
+    right_neighbor = (rank + 1) % world_size
+
+    # Create Buffers for receiving q_h_i and q_g_i
+    recv_left_q_h_i = torch.zeros_like(q_h_i)
+    recv_right_q_h_i = torch.zeros_like(q_h_i)
+    recv_left_q_g_i = torch.zeros_like(q_g_i)
+    recv_right_q_g_i = torch.zeros_like(q_g_i)
+
+    # send q_h_i message to left neighbor async and receive from right neighbor sync
+    req_send_left_q_h_i = dist.isend(q_h_i, dst=left_neighbor)
+    req_recv_right_q_h_i = dist.recv(recv_right_q_h_i, src=right_neighbor)
+    req_send_left_q_h_i.wait()
+
+    # send q_h_i message to right neighbor async and receive from left neighbor sync
+    req_send_right_q_h_i = dist.isend(q_h_i, dst=right_neighbor)
+    req_recv_left_q_h_i = dist.recv(recv_left_q_h_i, src=left_neighbor)
+    req_send_right_q_h_i.wait()
+
+    # send q_g_i message to left neighbor async and receive from right neighbor sync
+    req_send_left_q_g_i = dist.isend(q_g_i, dst=left_neighbor)
+    req_recv_right_q_g_i = dist.recv(recv_right_q_g_i, src=right_neighbor)
+    req_send_left_q_g_i.wait()
+
+    # send q_g_i message to right neighbor async and receive from left neighbor sync
+    req_send_right_q_g_i = dist.isend(q_g_i, dst=right_neighbor)
+    req_recv_left_q_g_i = dist.recv(recv_left_q_g_i, src=left_neighbor)
+    req_send_right_q_g_i.wait()
+
+    dist.barrier()
+    return (req_recv_left_q_h_i,
+            req_recv_right_q_h_i), (req_recv_left_q_g_i, req_recv_right_q_g_i)
+
+
+def motef_worker(rank, world_size, model, train_loader, val_loader, epochs, gamma, eta, lambda_, C_alpha):
+    
+    setup(rank, world_size)
+    
+    # num_gpus = torch.cuda.device_count()
+
+    # avoid cuda for now for simplification
+    device = torch.device(f'cuda:{rank}' if torch.cuda.is_available() else 'cpu')
+    # if torch.cuda.is_available():
+    #     device = torch.device("cuda:0")  # All workers use the same GPU
+    #     torch.cuda.set_device(device)
+    # else:
+    #     device = torch.device("cpu")
+    model = model.to(device)
+    # model.cuda(rank) 
+    model = DDP(
+        model 
+        # device_ids=[device]
+    )
+    
+
+    print(f'motef_worker rank {rank}, and device {device}')
+
+    criterion = nn.CrossEntropyLoss()
+
+    # Initialize local states
+    x = torch.zeros_like(torch.cat([p.data.view(-1) for p in model.parameters()]))
+    h = torch.zeros_like(x)
+    g = torch.zeros_like(x)
+    v = torch.zeros_like(x)
+    m = torch.zeros_like(x)
+    q_h_i = torch.zeros_like(x)
+    q_g_i = torch.zeros_like(x)
+
+    # Define neighbors (assuming a ring topology for simplicity)
+    left_neighbor = (rank - 1) % world_size
+    right_neighbor = (rank + 1) % world_size
+    idxs_n = [left_neighbor, right_neighbor]
+    neighborStates = {left_neighbor:
+                          {"h": torch.zeros_like(x), "g": torch.zeros_like(x)},
+                      right_neighbor:
+                          {"h": torch.zeros_like(x), "g": torch.zeros_like(x)}}
+    weights = torch.empty(world_size, world_size)
+    nn.init.zeros_(weights)
+    # TODO: fill weights with 1 ones for neighbors and keep 0 for the rest
+
+    # time the experiment
+    start_time = time.time()
+    ##############################################################
+    #                   start training routine                   #
+    ##############################################################
+    
+    model.train()
+    for epoch in range(epochs):
+        print(f'worker {rank} at epoch {epoch}, with device {device}')
+        train_loader.sampler.set_epoch(epoch)
+        print(f'Loader finished')
+        
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = data.to(device), target.to(device)
+            print(f'Heeeey, shit\'s happening')
+            # Receive q_h and q_g from neighbors
+
+            (q_h_i_left, q_h_i_right), (q_g_i_left, q_g_i_right) = communicate_with_neighbors(rank, world_size, q_h_i, q_g_i)
+
+            # update local neighbor states
+            neighborStates[left_neighbor]["h"] += q_h_i_left
+            neighborStates[left_neighbor]["g"] += q_g_i_left
+
+            neighborStates[right_neighbor]["h"] += q_h_i_right
+            neighborStates[right_neighbor]["g"] += q_g_i_right
+
+            # Update x
+            weighted_diffs = [weights[rank][x] * (neighborStates[x]["h"] - h) for x in idxs_n]
+            mixing = sum(weighted_diffs)
+            x += gamma * mixing - eta * v
+
+            # Compute q_h
+            q_h_i = C_alpha * (x - h)
+            h += q_h_i
+
+            # Compute gradient
+            model.zero_grad()
+            output = model(data)
+            loss = criterion(output, target)
+            loss.backward()
+            grad = torch.cat([p.grad.data.view(-1) for p in model.parameters()])
+
+            # Update m and v
+            m_old = m.clone()
+            m = (1 - lambda_) * m + lambda_ * grad
+            weighted_diffs_glob_grad = [weights[rank][x] * (neighborStates[x]["g"] - g) for x in idxs_n]
+            mixing_glob_grad = sum(weighted_diffs_glob_grad)
+            v += gamma * mixing_glob_grad + m - m_old
+
+            # Compute q_g
+            q_g_i = C_alpha * (v - g)
+            g += q_g_i
+
+            if batch_idx % 50 == 0: # and rank == 0:
+                print(f"Rank {rank}, Epoch {epoch + 1}, Batch {batch_idx}, Loss: {loss.item():.3f}")
+
+            # Update model parameters with x
+            param_shapes = [p.shape for p in model.parameters()]
+            param_numels = [p.numel() for p in model.parameters()]
+
+            with torch.no_grad():
+                x_split = x.split(param_numels)
+                for param, x_i, shape in zip(model.parameters(), x_split, param_shapes):
+                    param.data = x_i.view(shape)
+
+        # Evaluate on validation set
+        model.eval()
+        val_loss = 0
+        val_correct = 0
+        val_total = 0
+        with torch.no_grad():
+            for data, target in val_loader:
+                data, target = data.to(device), target.to(device)
+                output = model(data)
+                val_loss += criterion(output, target).item()
+
+                # Calculate accuracy
+                _, predicted = torch.max(output.data, 1)
+                val_total += target.size(0)
+                val_correct += (predicted == target).sum().item()
+
+        val_loss /= len(val_loader)
+        val_acc = val_correct / val_total
+
+        epoch_time = time.time() - start_time
+        print(
+            f"Rank {rank}, Epoch {epoch + 1}, Val Loss: {val_loss:.4f}, Val Acc: {val_acc:.4f}, Time: {epoch_time:.2f}s")
+
+        start_time = time.time()
+
+
+
+    cleanup()
+
+
+def worker_fn(rank, world_size, model, trainset, valset, epochs, gamma, eta, lambda_, C_alpha):
+    train_sampler = torch.utils.data.distributed.DistributedSampler(
+        trainset, num_replicas=world_size, rank=rank, shuffle=True)
+
+    train_loader = DataLoader(trainset, batch_size=64,
+                              num_workers=2, sampler=train_sampler)
+
+    val_loader = DataLoader(valset, batch_size=64, shuffle=False)
+
+    motef_worker(rank, world_size, model, train_loader, val_loader, epochs, gamma, eta, lambda_, C_alpha)
+
+
+def run_motef(world_size, epochs, gamma, eta, lambda_, C_alpha):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    valset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+
+    model = ResNet8()
+    # model = ResNet8().cpu()
+    # model = DDP(ResNet8)
+
+    model.share_memory()
+
+    mp.spawn(
+        worker_fn,
+        args=(world_size, model, trainset, valset, epochs, gamma, eta, lambda_, C_alpha),
+        nprocs=world_size
+    )
+
+
+if __name__ == "__main__":
+    
+    world_size = int(os.environ['SLURM_NTASKS'])  # Number of nodes
+    start_time = time.time()
+    run_motef(world_size=world_size, epochs=10, gamma=0.1, eta=0.01, lambda_=0.9, C_alpha=0.5)
+    # if torch.cuda.is_available():
+    #     torch.cuda.synchronize()
+    total_time = time.time() - start_time
+    print(f"Total execution time: {total_time:.2f}s")
+
+
+
+
+
+
+
+# dist.barrier()
+# torchmetrics (lightning ai)
